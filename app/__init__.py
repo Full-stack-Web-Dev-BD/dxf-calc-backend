3b39d1c0feb4a03afb142f516480c1963f2a3b36
alamin <alaminprogramerr@gmail.com>
2025-02-22 08:30:13 +0600

    adde initialization of  calc / user

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,8 @@ def create_app():
     app = Flask(__name__)
 
     # Import and register the user and product blueprints
-    from app.user import user_blueprint
-    from app.calc import calc_blueprint
+    from app.user.user import user_blueprint
+    from app.calc.calc import calc_blueprint
     app.register_blueprint(user_blueprint)
     app.register_blueprint(calc_blueprint)
     
