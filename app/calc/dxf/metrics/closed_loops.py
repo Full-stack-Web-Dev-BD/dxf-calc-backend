c2222b4c29d6f4514a56d1af8116bb16ca4af270
alamin <alaminprogramerr@gmail.com>
2025-02-22 08:34:44 +0600

    init

---
+++ app/calc/dxf/metrics/closed_loops.py
@@ -0,0 +1,33 @@
+# Cloosed loops count calculation
+
+loops = 0
+
+
+def closed_loops():
+	return loops
+
+
+def increase_closed_loops(count = 1):
+	global loops
+	loops += count
+	# print(f"[Closed loops:] {loops}")
+
+
+def calc_closed_loops_line(shape):
+	increase_closed_loops(0)
+	
+
+def calc_closed_loops_polyline(shape):
+	increase_closed_loops(1 if shape.is_closed else 0)
+
+
+def calc_closed_loops_spline(shape):
+	increase_closed_loops(1 if shape.closed else 0)
+
+
+def calc_closed_loops_circle(shape):
+	increase_closed_loops()
+
+
+def calc_closed_loops_arc(shape):
+	increase_closed_loops(0)
