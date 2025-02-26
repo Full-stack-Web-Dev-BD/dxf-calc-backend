c68351ec92b9e4855b1e308a4a8bc5730b22b59d
alamin <alaminprogramerr@gmail.com>
2025-02-26 18:12:33 +0600

    dxf filew viewr on progress

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
+import os
 from app import create_app
 from flask_cors import CORS
+from flask import send_from_directory, Flask
 
 app = create_app()
 
 # Enable CORS for all routes
 CORS(app)
 
-# Serve 'uploads' directory as static
-app.config['UPLOAD_FOLDER'] = 'uploads'
-app.static_folder = 'uploads'
+# Set absolute path for the upload folder
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Route to serve uploaded files
+@app.route('/')
+def index():
+    return '<h3 >Welcome to FastCNC! </h3>'
+
+@app.route('/uploads/<path:filename>')
+def serve_uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
+    print(f"Serving files from: {UPLOAD_FOLDER}")  # Debugging
     app.run(debug=True)
